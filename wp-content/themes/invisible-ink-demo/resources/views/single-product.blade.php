4991e458650995a0843867f12d7ead760e0cdefd
Jay <me@jirayu.in.th>
2025-01-13 11:31:09 +0700

    single product

--- wp-content/themes/invisible-ink-demo/resources/views/single-product.blade.php
+++ wp-content/themes/invisible-ink-demo/resources/views/single-product.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
-  @endwhile
+  <div class="container">
+    <div class="product__wrapper">
+      @while(have_posts()) @php(the_post())
+      <div class="product__photo">
+        @php(the_post_thumbnail('large'))
+      </div>
+      <div class="product__detail">
+        <div class="product__meta">
+          <h1 class="product__title">@php( the_title() )</h1>
+          <h3 class="product__brand">{{ get_field('brand') }}</h3>
+          <h3 class="product__price">S${{ get_field('price') }}</h3>
+        </div>
+        <div class="product__description">
+        @php( the_content() )
+        </div>
+      </div>
+      @endwhile
+    </div>
+  </div>
 @endsection
